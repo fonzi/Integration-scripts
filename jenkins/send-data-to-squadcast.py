2983cfa629caeb68485b34560dd7bb81c9c661fb
roshan8 <roshan.aloor@gmail.com>
2020-01-16 09:38:16 +0530

    Checking the last build status code

--- jenkins/send-data-to-squadcast.py
+++ jenkins/send-data-to-squadcast.py
@@ -8,6 +8,7 @@ import os
 import json
 import urllib.request
 import argparse
+import sys
 
 def form_payload(build_id, build_number, job_name, build_url):
     """Forms the python representation of the data payload to be sent from the passed configuration"""
@@ -28,6 +29,23 @@ def post_to_url(url, payload):
         if e.code >= 400:
             print("Some error occured while processing the event, ", e)
 
+def get_job_status(job_url):
+    try:
+        jenkins_stream = urllib.request.Request(job_url + "lastBuild/api/json")
+        build_status_json = json.load( jenkins_stream )
+
+        if build_status_json.has_key( "result" ):   
+            print (build_status_json["result"])
+
+    except urllib.request.URLError as e:
+        print ("URL Error: " + str(e.code) )
+        sys.exit(2)
+
+    
+
+def get_prevjob_status():
+    pass
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Passing build information.')
     parser.add_argument('--url', help='Squadcast API endpoint')
@@ -35,8 +53,11 @@ if __name__ == "__main__":
     parser.add_argument('--build_number', type=int, help='Build number of the pipeline')
     parser.add_argument('--job_name', help='Job name of the pipeline')
     parser.add_argument('--build_url', help='URL of the pipeline job')
+    parser.add_argument('--job_url', help='URL of the pipeline job')
 
     args = parser.parse_args()    
+
+    cur_job_status = get_job_status(args.job_url)
 
     print("Sending data to squadcast")
     post_to_url(args.url, form_payload(args.build_id, str(args.build_number), args.job_name, args.build_url ))
